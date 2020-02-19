35db7596b956b02f3cd3cfb5a995aea8df3021b0
John Mwashuma <jmwashuma@live.com>
2020-02-19 07:58:54 +0300

    Fetch image classifier model

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
+import * as mobilenet from "@tensorflow-models/mobilenet";
 import './App.css';
 
 const stateMachine = {
@@ -16,10 +17,39 @@ const stateMachine = {
 const reducer = (currentState, event) => stateMachine.states[currentState].on[event] || stateMachine.initial;
 
 function App() {
+  // app state reducer
   const [state, dispatch] = useReducer(reducer, stateMachine.initial);
+
+  // dispatch function
+  const next = () => dispatch('next');
+
+  // model local state, default value is null
+  const [model,setModel] = useState(null);
+
+  // load image classifier model
+  const loadModel = async () => {
+    // trigger state transition from initial to loadingModel
+    next();
+    // load model
+    const mobilenetModel = await mobilenet.load();
+    // set model to state
+    setModel(mobilenetModel);
+    // trigger state transition from loadingModel to awaitingUpload
+    next();
+  }
+
+  const buttonProps = {
+    initial: { text: 'Load Model', action: loadModel },
+    loadingModel: { text: 'Loading Model...', action: () => { }  },
+    awaitingUpload: { text: 'Upload Photo', action: () => { }  },
+    ready: { text: 'Identify', action: () => { }  },
+    classifying: { text: 'Identifying...', action: () => { }  },
+    complete: { text: 'Reset', action: () => { }  }
+  }
+
   return (
     <div>
-      <button onClick={() => dispatch('next')}>{state}</button>
+      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
     </div>
   );
 }
