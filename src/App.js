c0ae861863d6a48d0a39d1bf5fd48bfc084ed1c5
John Mwashuma <jmwashuma@live.com>
2020-02-19 08:16:21 +0300

    Add image upload logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer, useState, useRef } from 'react';
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import './App.css';
 
@@ -23,19 +23,38 @@ function App() {
   // dispatch function
   const next = () => dispatch('next');
 
-  // model local state, default value is null
+  // model local state, default null
   const [model,setModel] = useState(null);
+
+  // image url local state, default null
+  const [imageUrl,setImageUrl] = useState(null);
+
+  // image input ref
+  const inputRef = useRef();
 
   // load image classifier model
   const loadModel = async () => {
-    // trigger state transition from initial to loadingModel
+    // trigger state transition from initial to loadingModel state
     next();
     // load model
     const mobilenetModel = await mobilenet.load();
     // set model to state
     setModel(mobilenetModel);
-    // trigger state transition from loadingModel to awaitingUpload
+    // trigger state transition from loadingModel state to awaitingUpload state
     next();
+  }
+
+  // handle image upload
+  const handleImageUpload = e => {
+    const { files } = e.target;
+    
+    if (files.length > 0) {
+      // take the first file and get it's url
+      const url = URL.createObjectURL(files[0]);
+      setImageUrl(url);
+      // transition from awaitingupload state to ready state
+      next()
+    }
   }
 
   const buttonProps = {
@@ -49,7 +68,16 @@ function App() {
 
   return (
     <div>
-      <button onClick={buttonProps[state].action}>{buttonProps[state].text}</button>
+      <input
+        type="file"
+        accept="image/*"
+        capture="camera"
+        ref={inputRef}
+        onChange={handleImageUpload}
+      />
+      <button onClick={buttonProps[state].action}>
+        {buttonProps[state].text}
+      </button>
     </div>
   );
 }
