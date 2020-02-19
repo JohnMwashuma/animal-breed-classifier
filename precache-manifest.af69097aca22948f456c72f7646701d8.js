9db532e40ce2c5ad2e35cdc7a043a70ef21f5be0
John Mwashuma <jmwashuma@live.com>
2020-02-19 17:35:52 +0300

    Updates

---
+++ precache-manifest.af69097aca22948f456c72f7646701d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3fb1dbaa39f71e886020f394afec9e",
+    "url": "/animal-breed-classifier/index.html"
+  },
+  {
+    "revision": "c069d4a171f24b2cf1df",
+    "url": "/animal-breed-classifier/static/css/main.1d1af5da.chunk.css"
+  },
+  {
+    "revision": "0e90e771215212127f7d",
+    "url": "/animal-breed-classifier/static/js/2.aa25f496.chunk.js"
+  },
+  {
+    "revision": "7d1906d202e927039094444d5b221699",
+    "url": "/animal-breed-classifier/static/js/2.aa25f496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c069d4a171f24b2cf1df",
+    "url": "/animal-breed-classifier/static/js/main.44ce8a49.chunk.js"
+  },
+  {
+    "revision": "df15338091564284d0ef",
+    "url": "/animal-breed-classifier/static/js/runtime-main.8761ce1e.js"
+  }
+]);
