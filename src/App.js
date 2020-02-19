fbde5b0a1373d52b91b1ccfe803a9c68a220ab0f
John Mwashuma <jmwashuma@live.com>
2020-02-19 07:08:39 +0300

    Refactor code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 
 const stateMachine = {
-  initial: { on: { next: 'loadingModel' } },
-  loadingModel: { on: { next: 'awaitingUpload' } },
-  awaitingUpload: { on: { next: 'ready' } },
-  ready: { on: { next: 'classifying' } },
-  classifying: { on: { next: 'complete' } },
-  complete: { on: { next: 'awaitingUpload' } },
+  initial: 'initial',
+  states: {
+    initial: { on: { next: 'loadingModel' } },
+    loadingModel: { on: { next: 'awaitingUpload' } },
+    awaitingUpload: { on: { next: 'ready' } },
+    ready: { on: { next: 'classifying' } },
+    classifying: { on: { next: 'complete' } },
+    complete: { on: { next: 'awaitingUpload' } }
+  }
 }
 
 function App() {
